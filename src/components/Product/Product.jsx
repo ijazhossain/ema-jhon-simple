17238bdd5b9bc8c4790b61dcb5611fddea72a85e
ijaz35 <ijaz.hossain1@gmail.com>
2023-03-27 13:51:35 +0600

    handler add to cart btn

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    console.log()
+    // console.log(props.handleAddTOCart);
     const { id, img, name, price, seller, ratings } = props.product;
-    console.log(id, img, name, price, seller, ratings);
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product-container'>
 
@@ -18,13 +21,12 @@ const Product = (props) => {
                 <p><small>Manufacturer: {seller}</small></p>
                 <p><small>Rating: {ratings} stars</small></p>
             </div>
-            <button>
+            <button onClick={() => handleAddToCart(props.product)}>
 
 
-                Add to cart
-                <svg style={{ width: '18px', marginLeft: '6px' }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg></button>
+                <span> Add to cart</span>
+                <FontAwesomeIcon style={{ marginLeft: '10px' }} icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
